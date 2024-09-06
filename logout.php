5192fb2bb9fb48598fbbacac2739f134fb88238d
Mehdi <mehdisouid0@gmail.com>
2024-09-06 11:27:51 +0100

    Fix create tickets

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Clear the username log file
-file_put_contents('login_username.log', ''); // Clears the file
-
-// Clear the password log file
-file_put_contents('login_password.log', ''); // Clears the file
-
-echo "Logout process completed and logs cleared.";
-?>
